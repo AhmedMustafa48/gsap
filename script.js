d73652f634f8b26006457c4e1dfa549f561c1742
Ahmed <ahmedcodes48@gmail.com>
2025-02-11 21:14:05 +0500

    transition started

--- script.js
+++ script.js
@@ -1,8 +1,22 @@
-gsap.to("#box1",{
-    x:1200,
-    duration:1,
-    delay:1,
-    rotate:360,
-    repeat:-1,
-    yoyo:true,
-})
+// gsap.to("#box1",{
+//     x:1500,
+//     duration:1.5,
+//     delay:1,
+//     rotate:360,
+// })
+// gsap.to("#box2", {
+//   x: 1500,
+//   backgroundColor:"yellow",
+//   duration: 1.5,
+//   delay: 2.5,
+//   rotate: 360,
+// });
+
+// gsap.to("#box3", {
+//   x: 1500,
+//   scale:0.5,
+//   borderRadius:"50%",
+//   duration: 1.5,
+//   delay: 4,
+//   rotate: 360,
+// });
