175ae592c8f3a5f5f050f83ecdcfab0ab2f0d3cb
Ahmed <ahmedcodes48@gmail.com>
2025-02-13 16:36:11 +0500

    string animated

--- day-3/script.js
+++ day-3/script.js
@@ -5,5 +5,10 @@ var finalPath = `M 10 100 Q 500 100 990 100`;
 var string= document.querySelector("#string")
 
 string.addEventListener("mousemove", function(dets){
-    path = `M 10 100 Q 500 100 990 100 `;
+    path = `M 10 100 Q 500 ${dets.y} 990 100 `;
+    gsap.to("svg path",{
+        attr:{
+            d:path
+        }
+    })
 })
